808e3e9d68bcd5ccdfaaefb74ac0839e2c7e0ec1
Avika <avzin07@gmail.com>
2026-04-13 22:33:25 +0530

    connected to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
